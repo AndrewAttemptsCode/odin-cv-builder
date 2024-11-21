7b26b1489fb561f0fdca67dc368832081395b34a
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-11-21 15:39:58 +0000

    feat: add main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 
 export default function App() {
   return (
     <>
       <Header />
-      <main>
-
-      </main>
+      <Main />
     </>
   );
 }
