9222b6abde48f1976893537d7c28d914201f4da5
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-11-21 17:32:48 +0000

    feat: add personal info section data storage

---
+++ src/components/Main/data.js
@@ -0,0 +1,11 @@
+export const cvData = [
+  {
+    personalInfo: {
+      firstName: "Andrew",
+      lastName: "Attempts",
+      location: "Manchester, UK",
+      email: "hello@test.co.uk",
+      github: "AndrewAttemptsCode",
+    }
+  }
+]
