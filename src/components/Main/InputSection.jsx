c9da48cfd3c04b0c568693f0222d0db51b39602e
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-11-23 20:06:01 +0000

    feat: add Education section component

--- src/components/Main/InputSection.jsx
+++ src/components/Main/InputSection.jsx
@@ -1,9 +1,11 @@
+import Education from "./Education";
 import PersonalInfo from "./PersonalInfo";
 
 export default function InputSection() {
   return (
     <section className="inputSection">
       <PersonalInfo />
+      <Education />
     </section>
   );
 }
