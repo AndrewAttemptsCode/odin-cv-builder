66b939a1c85b8b87c9dca3fe88b3ebac855d9f27
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-11-23 20:05:12 +0000

    feat: add education section

--- src/components/Main/data.js
+++ src/components/Main/data.js
@@ -6,6 +6,16 @@ export const cvData = [
       location: "Manchester, UK",
       email: "hello@test.co.uk",
       github: "AndrewAttemptsCode",
-    }
+    },
+    education: [
+      {
+        id: 0,
+        schoolName: "University of Odin",
+        location: "Asgard",
+        startDate: "Sept 2022",
+        endDate: "Jul 2024",
+        title: "Gate Guardian",
+      }
+    ]
   }
 ]
