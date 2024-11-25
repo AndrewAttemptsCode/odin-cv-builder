71d03b551653dde7f582160b2d5a0be456dd7a75
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-11-25 19:53:36 +0000

    feat: add work experience dummy data

--- src/components/Main/data.js
+++ src/components/Main/data.js
@@ -16,6 +16,21 @@ export const cvData = [
         endDate: "Jul 2024",
         title: "Gate Guardian",
       }
-    ]
-  }
-]
+    ],
+    workExperience: [
+      {
+        id: 0,
+        companyName: "Bifrost Security Inc.",
+        position: "Junior Gatekeeper",
+        location: "Asgard",
+        startDate: "Sept 2023",
+        endDate: "Jul 2024",
+        responsibilities: [
+          "Monitored and regulated access to the Bifrost portal.",
+          "Collaborated with senior gatekeepers to resolve inter-realm disputes.",
+          "Implemented advanced Asgardian authentication protocols.",
+        ],
+      },
+    ],
+  },
+];
