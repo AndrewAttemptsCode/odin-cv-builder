f80679b9d140755bd1b899b5c73416fdc2063264
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-11-25 19:53:15 +0000

    feat: add work experience component

--- src/components/Main/InputSection.jsx
+++ src/components/Main/InputSection.jsx
@@ -1,11 +1,13 @@
 import Education from "./Education";
 import PersonalInfo from "./PersonalInfo";
+import WorkExperience from "./WorkExperience";
 
 export default function InputSection() {
   return (
     <section className="inputSection">
       <PersonalInfo />
       <Education />
+      <WorkExperience />
     </section>
   );
 }
