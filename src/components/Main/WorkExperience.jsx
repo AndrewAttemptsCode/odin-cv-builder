d2c4ff21cf3b831df57c29a6242078bb89416636
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-11-26 21:25:44 +0000

    fix: add empty array item

--- src/components/Main/WorkExperience.jsx
+++ src/components/Main/WorkExperience.jsx
@@ -31,7 +31,7 @@ export default function WorkExperience() {
         position: "",
         startDate: "",
         endDate: "",
-        responsibilities: [],
+        responsibilities: [""],
       }
     ])
   }
