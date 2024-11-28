4d475975a78d5aaa80553ea2a027ad5be3febd25
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-11-28 18:02:10 +0000

    feat: add personal info section

--- src/components/Main/CvPreview.jsx
+++ src/components/Main/CvPreview.jsx
@@ -3,7 +3,11 @@ import "../../styles/CvPreview.css";
 export default function CvPreview({ personalInfo }) {
   return (
     <div className="cv-preview">
-      <h1>{personalInfo.firstName} {personalInfo.lastName}</h1>
+      <section className="cv-personal-info">
+        <h1 className="cv-fullname">{personalInfo.firstName} {personalInfo.lastName}</h1>
+        <p>{personalInfo.location} | {personalInfo.email} | github.com/{personalInfo.github}</p>
+      </section>
+      
     </div>
   );
 }
