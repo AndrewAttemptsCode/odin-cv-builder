09bba99d1b9269c6199c3960f68af67c4124353b
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-11-28 14:59:55 +0000

    feat: add preview section component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,11 @@
 import InputSection from "./InputSection";
+import PreviewSection from "./PreviewSection";
 
 export default function Main() {
   return (
     <main>
       <InputSection />
+      <PreviewSection />
     </main>
   );
 }
