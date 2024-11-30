b426bb0590aa70bbfe3329b0f9daac85cd6775a3
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-11-30 20:32:46 +0000

    feat: add education section

--- src/components/Main/CvPreview.jsx
+++ src/components/Main/CvPreview.jsx
@@ -1,11 +1,32 @@
 import "../../styles/CvPreview.css";
 
-export default function CvPreview({ personalInfo }) {
+export default function CvPreview({ personalInfo, educationList }) {
   return (
     <div className="cv-preview">
       <section className="cv-personal-info">
         <h1 className="cv-fullname">{personalInfo.firstName} {personalInfo.lastName}</h1>
         <p>{personalInfo.location} | {personalInfo.email} | github.com/{personalInfo.github}</p>
+      </section>
+
+      <section className="cv-education-info">
+        <h2>Education</h2>
+        <hr className="thick-rule" />
+        {educationList.map((educationItem, index) => (
+          <div key={educationItem.id} className="cv-education-item">
+            <div className="cv-education-header">
+              <div className="cv-education-name">
+                <h3>{educationItem.schoolName}</h3>
+                <span>-</span>
+                <p>{educationItem.location}</p>
+              </div>
+              <div className="cv-education-dates">
+                <p>{educationItem.startDate} - {educationItem.endDate}</p>
+              </div>
+            </div>
+            <p>{educationItem.title}</p>
+            {index < educationList.length - 1 && <hr />}
+          </div>
+        ))}
       </section>
       
     </div>
