f29a1e10c6ba50c087754aa21db3c1b52e42911c
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-12-01 18:11:44 +0000

    feat: add additional dummy data

--- src/components/Main/data.js
+++ src/components/Main/data.js
@@ -15,7 +15,15 @@ export const cvData = [
         startDate: "Sept 2022",
         endDate: "Jul 2024",
         title: "Gate Guardian",
-      }
+      },
+      {
+        id: 1,
+        schoolName: "Midgard Technical College",
+        location: "Earth",
+        startDate: "Sept 2020",
+        endDate: "Jul 2022",
+        title: "Diploma in Portal Engineering",
+      },
     ],
     workExperience: [
       {
@@ -29,6 +37,19 @@ export const cvData = [
           "Monitored and regulated access to the Bifrost portal.",
           "Collaborated with senior gatekeepers to resolve inter-realm disputes.",
           "Implemented advanced Asgardian authentication protocols.",
+        ],
+      },
+      {
+        id: 1,
+        companyName: "Yggdrasil Network Solutions",
+        position: "Apprentice Network Engineer",
+        location: "Midgard",
+        startDate: "Aug 2022",
+        endDate: "Aug 2023",
+        responsibilities: [
+          "Assisted in setting up secure inter-realm communication systems.",
+          "Maintained and upgraded branches of the Yggdrasil network.",
+          "Provided troubleshooting support for cross-realm latency issues.",
         ],
       },
     ],
